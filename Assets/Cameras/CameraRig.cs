0e358fde229ac7b27010ddac27c765572a084766
Chris Kim <christopherjkim@gmail.com>
2015-12-12 17:12:41 -0500

    Moved player character into prefab, added our standard camera to Pathfinding scene
    [git-p4: depot-paths = "//ld34/main/": change = 21]

--- Assets/Cameras/CameraRig.cs
+++ Assets/Cameras/CameraRig.cs
@@ -43,7 +43,6 @@ public class CameraRig : PivotBasedCameraRig
 
     protected void Update()
     {
-        HandleRotationMovement();
         if (m_LockCursor && Input.GetMouseButtonUp(0))
         {
             Cursor.lockState = m_LockCursor ? CursorLockMode.Locked : CursorLockMode.None;
@@ -67,49 +66,5 @@ public class CameraRig : PivotBasedCameraRig
     }
 
 
-    private void HandleRotationMovement()
-    {
-        if(Time.timeScale < float.Epsilon)
-            return;
-
-        // Read the user input
-        var x = CrossPlatformInputManager.GetAxis("Mouse X");
-        var y = CrossPlatformInputManager.GetAxis("Mouse Y");
-
-        // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
-        // m_LookAngle += x*m_TurnSpeed;
-
-        // Rotate the rig (the root object) around Y axis only:
-        m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
-
-        if (m_VerticalAutoReturn)
-        {
-            // For tilt input, we need to behave differently depending on whether we're using mouse or touch input:
-            // on mobile, vertical input is directly mapped to tilt value, so it springs back automatically when the look input is released
-            // we have to test whether above or below zero because we want to auto-return to zero even if min and max are not symmetrical.
-            m_TiltAngle = y > 0 ? Mathf.Lerp(0, -m_TiltMin, y) : Mathf.Lerp(0, m_TiltMax, -y);
-        }
-        else
-        {
-            // on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
-            m_TiltAngle -= y*m_TurnSpeed;
-            // and make sure the new value is within the tilt range
-            m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
-        }
-
-        // Tilt input around X is applied to the pivot (the child of this object)
-        m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y , m_PivotEulers.z);
-
-        if (m_TurnSmoothing > 0)
-        {
-            m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
-        }
-        else
-        {
-            m_Pivot.localRotation = m_PivotTargetRot;
-            transform.localRotation = m_TransformTargetRot;
-        }
-    }
 }
 }
