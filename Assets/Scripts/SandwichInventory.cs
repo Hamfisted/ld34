b91b11b7379431ae2480902be2888e39730c250f
Jordan Cristiano <jordan.cristi@gmail.com>
2015-12-12 19:24:26 -0500

    Sandwich pickups code and test. The character needs a SandwichInventory component and the pickups need a SandwichPickup component. The SandwichPickup component has a type that you edit from the editor (bread, cheese, meat, lettuce).
    [git-p4: depot-paths = "//ld34/main/": change = 28]

---
+++ Assets/Scripts/SandwichInventory.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandwichInventory : MonoBehaviour {
+
+	public enum SandwichPart
+	{
+		Bread = 0,
+		Cheese,
+		Meat,
+		Lettuce,
+		NumParts
+	};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool AcceptSandwichPart(SandwichPart type) {
+		if (type >= SandwichPart.NumParts) {
+			return(false);
+		}
+		int max = 1;
+		if (type == SandwichPart.Bread) {
+			max = 2;
+		}
+		if (mSandwichParts [(int)type] >= max) {
+			return(false);
+		}
+		mSandwichParts [(int)type] += 1;
+		if (HasEnoughPartsForSandwich ()) {
+			MakeSandwich ();
+		}
+		return(true);
+	}
+
+	bool HasEnoughPartsForSandwich() {
+		if ((mSandwichParts [(int)SandwichPart.Bread] >= 2) &&
+			(mSandwichParts [(int)SandwichPart.Cheese] >= 1) &&
+			(mSandwichParts [(int)SandwichPart.Meat] >= 1) &&
+			(mSandwichParts [(int)SandwichPart.Lettuce] >= 1)) {
+			return(true);
+		}
+		return(false);
+	}
+
+	void MakeSandwich() {
+		mSandwichParts.Initialize ();
+		mNumSandwiches += 1;
+	}
+
+	bool UseSandwich() {
+		if (mNumSandwiches > 0) {
+			mNumSandwiches -= 1;
+			return(true);
+		}
+		return(false);
+	}
+
+	int[] mSandwichParts = new int[(int)SandwichPart.NumParts];
+	int mNumSandwiches = 0;
+}
---
+++ Assets/Scripts/SandwichPartPickup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandwichPartPickup : MonoBehaviour {
+
+	[SerializeField] public SandwichInventory.SandwichPart mType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(Input.GetKeyDown(KeyCode.R))
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+		}
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		}*/
+	}
+
+	//==========================//
+	void OnTriggerEnter2D(Collider2D other) {
+		var inv = other.gameObject.GetComponent<SandwichInventory> ();
+		if (inv && inv.AcceptSandwichPart(mType)) {
+			Destroy (gameObject);
+		}
+	}
+}
