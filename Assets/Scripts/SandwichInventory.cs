82557550ddaba4f8efc96876b00ae57772c98ce5
Jordan Cristiano <jordan.cristi@gmail.com>
2015-12-14 00:36:00 -0500

    Added sandwich pickups that add values to SandwichInventory which is on the character.
    [git-p4: depot-paths = "//ld34/main/": change = 47]

--- Assets/Scripts/SandwichInventory.cs
+++ Assets/Scripts/SandwichInventory.cs
@@ -1,68 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SandwichInventory : MonoBehaviour {
+public class SandwichInventory : MonoBehaviour
+{
+    public enum SandwichPart
+    {
+        Bread = 0,
+        Cheese,
+        Meat,
+        Lettuce,
+        Tomato,
+        NumParts
+    };
+    [SerializeField] public int NumRequiredBread = 2;
+    [SerializeField] public int NumRequiredCheese = 1;
+    [SerializeField] public int NumRequiredMeat = 1;
+    [SerializeField] public int NumRequiredLettuce = 1;
+    [SerializeField] public int NumRequiredTomato = 1;
+    [SerializeField] public Object SandwichType;
 
-	public enum SandwichPart
-	{
-		Bread = 0,
-		Cheese,
-		Meat,
-		Lettuce,
-		NumParts
-	};
+    // Use this for initialization
+    void Start()
+    {
+        mSandwichParts = new int[(int)SandwichPart.NumParts];
+        mSandwichPartLimits = new int[(int)SandwichPart.NumParts];
+        mSandwichPartLimits[(int)SandwichPart.Bread] = NumRequiredBread;
+        mSandwichPartLimits[(int)SandwichPart.Cheese] = NumRequiredCheese;
+        mSandwichPartLimits[(int)SandwichPart.Meat] = NumRequiredMeat;
+        mSandwichPartLimits[(int)SandwichPart.Lettuce] = NumRequiredLettuce;
+        mSandwichPartLimits[(int)SandwichPart.Tomato] = NumRequiredTomato;
+        mNumSandwiches = 0;
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	public bool AcceptSandwichPart(SandwichPart type) {
-		if (type >= SandwichPart.NumParts) {
-			return(false);
-		}
-		int max = 1;
-		if (type == SandwichPart.Bread) {
-			max = 2;
-		}
-		if (mSandwichParts [(int)type] >= max) {
-			return(false);
-		}
-		mSandwichParts [(int)type] += 1;
-		if (HasEnoughPartsForSandwich ()) {
-			MakeSandwich ();
-		}
-		return(true);
-	}
+    public bool AcceptSandwichPart(SandwichPart type)
+    {
+        if (type >= SandwichPart.NumParts)
+        {
+            return (false);
+        }
+        if (mSandwichParts[(int)type] >= mSandwichPartLimits[(int)type])
+        {
+            return (false);
+        }
+        mSandwichParts[(int)type] += 1;
+        if (HasEnoughPartsForSandwich())
+        {
+            MakeSandwich();
+        }
+        return (true);
+    }
 
-	bool HasEnoughPartsForSandwich() {
-		if ((mSandwichParts [(int)SandwichPart.Bread] >= 2) &&
-			(mSandwichParts [(int)SandwichPart.Cheese] >= 1) &&
-			(mSandwichParts [(int)SandwichPart.Meat] >= 1) &&
-			(mSandwichParts [(int)SandwichPart.Lettuce] >= 1)) {
-			return(true);
-		}
-		return(false);
-	}
+    public bool UseSandwich()
+    {
+        if (mNumSandwiches > 0)
+        {
+            Instantiate(SandwichType, transform.position, transform.rotation);
+            mNumSandwiches -= 1;
+            return (true);
+        }
+        return (false);
+    }
 
-	void MakeSandwich() {
-		mSandwichParts.Initialize ();
-		mNumSandwiches += 1;
-	}
+    bool HasEnoughPartsForSandwich()
+    {
+        if ((mSandwichParts[(int)SandwichPart.Bread] >= mSandwichPartLimits[(int)SandwichPart.Bread]) &&
+            (mSandwichParts[(int)SandwichPart.Cheese] >= mSandwichPartLimits[(int)SandwichPart.Cheese]) &&
+            (mSandwichParts[(int)SandwichPart.Meat] >= mSandwichPartLimits[(int)SandwichPart.Meat]) &&
+            (mSandwichParts[(int)SandwichPart.Lettuce] >= mSandwichPartLimits[(int)SandwichPart.Lettuce]) &&
+            (mSandwichParts[(int)SandwichPart.Tomato] >= mSandwichPartLimits[(int)SandwichPart.Tomato]))
+        {
+            return (true);
+        }
+        return (false);
+    }
 
-	bool UseSandwich() {
-		if (mNumSandwiches > 0) {
-			mNumSandwiches -= 1;
-			return(true);
-		}
-		return(false);
-	}
+    void MakeSandwich()
+    {
+        mSandwichParts = new int[(int)SandwichPart.NumParts];
+        mNumSandwiches += 1;
+    }
 
-	int[] mSandwichParts = new int[(int)SandwichPart.NumParts];
-	int mNumSandwiches = 0;
+    int[] mSandwichParts;
+    int[] mSandwichPartLimits;
+    int mNumSandwiches;
 }
--- Assets/Scripts/SandwichPartPickup.cs
+++ Assets/Scripts/SandwichPartPickup.cs
@@ -1,36 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SandwichPartPickup : MonoBehaviour {
+public class SandwichPartPickup : MonoBehaviour
+{
+    [SerializeField] public SandwichInventory.SandwichPart mType;
 
-	[SerializeField] public SandwichInventory.SandwichPart mType;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		/*if(Input.GetKeyDown(KeyCode.R))
-		{
-			gameObject.GetComponent<Renderer>().material.color = Color.red;
-		}
-		if(Input.GetKeyDown(KeyCode.G))
-		{
-			gameObject.GetComponent<Renderer>().material.color = Color.green;
-		}
-		if(Input.GetKeyDown(KeyCode.B))
-		{
-			gameObject.GetComponent<Renderer>().material.color = Color.blue;
-		}*/
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	//==========================//
-	void OnTriggerEnter2D(Collider2D other) {
-		var inv = other.gameObject.GetComponent<SandwichInventory> ();
-		if (inv && inv.AcceptSandwichPart(mType)) {
-			Destroy (gameObject);
-		}
-	}
+    //==========================//
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var inv = other.gameObject.GetComponent<SandwichInventory>();
+        if (inv && inv.AcceptSandwichPart(mType))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
