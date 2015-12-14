873e77123db476643553f527b4ee15ec97b77654
Chris Kim <christopherjkim@gmail.com>
2015-12-14 17:27:19 -0500

    chomp animation w/ proper masking!!!
    [git-p4: depot-paths = "//ld34/main/": change = 63]

--- Assets/Scripts/SandwichEater.cs
+++ Assets/Scripts/SandwichEater.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 public class SandwichEater : MonoBehaviour
 {
     [SerializeField] public int MaxFat = 1;
+
+    protected Animator animator;
+
     // Use this for initialization
     void Start()
     {
         mFatLevel = 0;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +26,9 @@ public class SandwichEater : MonoBehaviour
         if(mFatLevel >= MaxFat)
         {
             Destroy(gameObject);
+        } else {
+            animator.SetTrigger("Chomp");
+            animator.SetLayerWeight(1, 1);
         }
     }
 
