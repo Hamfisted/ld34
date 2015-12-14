72669d02554326d4ab69d556b8207c171888db0e
Jordan Cristiano <jordan.cristi@gmail.com>
2015-12-14 15:57:31 -0500

    Added sandwich part sprites based on part type. All sandwich parts in the editor will show as bread.
    [git-p4: depot-paths = "//ld34/main/": change = 58]

--- Assets/Scripts/SandwichPartPickup.cs
+++ Assets/Scripts/SandwichPartPickup.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SandwichPartPickup : MonoBehaviour
 {
-    [SerializeField] public SandwichInventory.SandwichPart mType;
+    [SerializeField] public SandwichInventory.SandwichPart Type;
+    static SandwichResources resources;
 
     // Use this for initialization
     void Start()
     {
+        resources = FindObjectOfType<SandwichResources>();
+        mSprites = new Sprite[(int)SandwichInventory.SandwichPart.NumParts];
+        GetComponent<SpriteRenderer>().sprite = resources.SandwichSprites[(int)Type];
     }
 
     // Update is called once per frame
@@ -19,9 +24,11 @@ public class SandwichPartPickup : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         var inv = other.gameObject.GetComponent<SandwichInventory>();
-        if (inv && inv.AcceptSandwichPart(mType))
+        if (inv && inv.AcceptSandwichPart(Type))
         {
             Destroy(gameObject);
         }
     }
+
+    Sprite[] mSprites;
 }
---
+++ Assets/Scripts/SandwichResources.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SandwichResources : MonoBehaviour {
+
+    [SerializeField]
+    public Sprite[] SandwichSprites;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+}
