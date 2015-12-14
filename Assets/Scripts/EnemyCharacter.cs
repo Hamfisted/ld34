7d567da05797f0a7cdfd80b8d912f49e601708a3
Chris Kim <christopherjkim@gmail.com>
2015-12-14 16:39:25 -0500

    add run anim to EnemyPig -- start trying out animation mask
    [git-p4: depot-paths = "//ld34/main/": change = 61]

--- Assets/Scripts/EnemyCharacter.cs
+++ Assets/Scripts/EnemyCharacter.cs
@@ -5,8 +5,20 @@ using System.Collections.Generic;
 public class EnemyCharacter : Character
 {
     [SerializeField] public float JumpPeakOffset = 2f;
-    [SerializeField] private Character player;
+    [SerializeField] private PlayerCharacter player;
 
     // Enemy gameplay state.
     private GameObject target;
+
+    void Update()
+    {
+        UpdateAnimator();
+    }
+
+    // Per frame character update.
+    protected virtual void UpdateAnimator()
+    {
+        // animator.SetBool("OnGround", true);
+        // animator.SetTrigger("Chomp");
+    }
 }
