2719a50d5ec9f510d57494f06ac008c0c7ad0a4e
Evgenii Babinets <evgenii.babinets@gmail.com>
2015-12-14 22:24:41 -0500

    Added manual check for sandwich eating because the triggers get fucked up
    [git-p4: depot-paths = "//ld34/main/": change = 98]

--- Assets/Scripts/SandwichPickup.cs
+++ Assets/Scripts/SandwichPickup.cs
@@ -13,6 +13,19 @@ public class SandwichPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // LOL
+        EnemyCharacter character = FindObjectOfType<EnemyCharacter>();
+        SandwichEater eater = FindObjectOfType<SandwichEater>();
+        if (character && eater)
+        {
+            Vector2 pos = transform.position;
+            Vector2 diff = (character.GetBodyPosition() - pos);
+            if (Mathf.Abs(diff.x) < 2f * character.gameObject.transform.localScale.x && Mathf.Abs(diff.y) < 4f * character.gameObject.transform.localScale.y)
+            {
+                eater.EatSandwich();
+                Destroy(gameObject);
+            }
+        }            
     }
 
     void OnTriggerEnter2D(Collider2D other)
